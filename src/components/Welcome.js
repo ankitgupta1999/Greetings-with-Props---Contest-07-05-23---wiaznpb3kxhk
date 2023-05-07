3ae46020ea2ae4103abcf225349365698fc376c5
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-05-07 07:16:33 +0000

    reactadded

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 // write code for Welcome component here
+import React from "react";
 export default function Welcome({name}){
     return (
         <>
         <h1>Hey {name}!</h1>
-      
         <h2>Welcome to Newton School.</h2>
         </>
     )
